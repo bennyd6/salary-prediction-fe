29ba63be2efa24ff26fde5880f5a3fb277e0b77c
bennyd6 <228r1a66d6@cmrec.ac.in>
2025-02-15 23:47:33 +0530

    changed api

--- src/components/SalaryCalculator.tsx
+++ src/components/SalaryCalculator.tsx
@@ -32,7 +32,7 @@ export function SalaryCalculator({ onClose }: SalaryCalculatorProps) {
     await fetchExchangeRate(data.currency); // Fetch exchange rate for conversion
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://salary-prediction-ml.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
